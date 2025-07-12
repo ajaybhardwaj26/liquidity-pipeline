74c78f76f38988bc7c5da0ad26aed99a100c5d66
Ajay <ajayb.shr@gmail.com>
2025-07-12 09:05:39 +0100

    first attempt

--- spark_jobs/calculate_liquidity_metrics.py
+++ spark_jobs/calculate_liquidity_metrics.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lag, to_timestamp
+from pyspark.sql.functions import col, lag, to_timestamp, expr
 from pyspark.sql.window import Window
-
 def read_raw_data(spark, s3_path):
     return spark.read.format("csv") \
         .option("header", "true") \
@@ -9,7 +8,7 @@ def read_raw_data(spark, s3_path):
         .load(s3_path)
 
 def clean_and_transform(df):
-    df = df.withColumn("timestamp", to_timestamp("timestamp"))
+    df = df.withColumn("timestamp", to_timestamp("timestamp", "M/d/yyyy H:mm"))
 
     window_spec = Window.partitionBy("symbol").orderBy("timestamp")
 
@@ -23,12 +22,18 @@ def write_to_s3(df, output_path):
     df.write.mode("overwrite").parquet(output_path)
 
 def main():
-    s3_input = "s3://liquidity-pipeline-data/raw/market_feed/market_feed_sample.csv"
-    s3_output = "s3://liquidity-pipeline-data/processed/liquidity_metrics/"
+    s3_input = "s3a://liquidity-pipeline-data/raw/market_feed/market_feed_sample.csv"
+    s3_output = "s3a://liquidity-pipeline-data/processed/liquidity_metrics/"
 
     spark = SparkSession.builder.appName("LiquidityPipeline").getOrCreate()
 
     raw_df = read_raw_data(spark, s3_input)
+
+    rows = raw_df.select("timestamp").collect()
+    for row in rows:
+        print(f"Raw timestamp value: {row['timestamp']}")
+
+
     processed_df = clean_and_transform(raw_df)
     write_to_s3(processed_df, s3_output)
 
